574ebe6e37f92d494c5b225236cb89427a734be8
Alvaro Silva <Alvaro Silva>
2016-10-02 19:26:36 +0200

    Modal for error messages

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,12 +2,14 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var OpenWeatherMap = require('OpenWeatherMap');
+var ErrorModal = require('ErrorModal');
 
 var Weather = React.createClass({
     
     getInitialState: function() {
       return {
-          isLoading: false
+          isLoading: false,
+          
       }  
     },
     
@@ -15,7 +17,8 @@ var Weather = React.createClass({
             var that = this;
         
             this.setState({
-                isLoading: true
+                isLoading: true,
+                errorMessage: undefined
             });
         
         OpenWeatherMap.getTemp(location).then(
@@ -26,16 +29,18 @@ var Weather = React.createClass({
                 isLoading: false
             });        
             },
-            function(errorMessage) {
-                that.setState({isLoading: false});
-                alert(errorMessage);
+            function(e) {
+                that.setState({
+                    isLoading: false,
+                    errorMessage: e.message
+                });
             }
         )
     },
     
     render: function() {
         
-        var {isLoading, temp, location} = this.state;
+        var {isLoading, temp, location, errorMessage} = this.state;
         
         function renderMessage() {
             if(isLoading) {
@@ -45,11 +50,20 @@ var Weather = React.createClass({
             }
         }
         
+        function renderError (){
+            if(typeof errorMessage === 'string') {
+                return (
+                    <ErrorModal message={errorMessage}/>
+                );
+            }
+        }
+        
         return (
             <div>
             <h1 className="text-center">Get Weather</h1>
             <WeatherForm onSearch={this.handleSearch}/>
             {renderMessage()}
+            {renderError()}
             </div>
         );
     }
